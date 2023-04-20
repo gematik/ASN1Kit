78eb1a449e6d9dbecc263ccc5cb5133a62350e62
sigabrtz <56683297+sigabrtz@users.noreply.github.com>
2023-04-20 16:39:07 +0200

    Use Docc for API documentation
    Changelog: other

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -28,7 +28,8 @@ let package = Package(
         .package(url: "http://github.com/Quick/Nimble", from: "9.2.0"),
         .package(url: "https://github.com/SwiftCommon/DataKit.git", from: "1.1.0"),
         .package(url: "http://github.com/Carthage/Commandant", from: "0.17.0"),
-        .package(name: "GemCommonsKit", url: "https://github.com/gematik/ref-GemCommonsKit", .branch("1.2.0"))
+        .package(name: "GemCommonsKit", url: "https://github.com/gematik/ref-GemCommonsKit", .branch("1.2.0")),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -36,7 +37,7 @@ let package = Package(
         .target(
             name: "ASN1Kit",
             dependencies: ["DataKit", "GemCommonsKit"]),
-        .target(
+        .executableTarget(
             name: "CLI",
             dependencies: ["ASN1Kit", "Commandant"]),
         .testTarget(
